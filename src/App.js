ef3e0d052e515652068b2d4815c3488ebc46c6cc
Tim Spratt <timothyspratt88@gmail.com>
2023-07-19 21:59:43 -0500

    added my new files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from "react";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
+
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-      <span>{10+16}</span>
-    </div>
+    <>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
+    </>
   );
 }
 
